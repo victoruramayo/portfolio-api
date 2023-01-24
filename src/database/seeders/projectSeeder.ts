7053aadb0365317f3fb945a0078d4e97b77c4199
victor ulises <vramayo@cuicker.com>
2023-01-23 22:42:03 -0500

    Add in projectSeeder the portfolio field

--- src/database/seeders/projectSeeder.ts
+++ src/database/seeders/projectSeeder.ts
@@ -2,6 +2,7 @@ import { Seeder, SeederFactoryManager } from 'typeorm-extension';
 import { DataSource } from 'typeorm';
 import { Profile } from '../../auth/models/entities/profile.entity';
 import { Project } from '../../portfolio/models/entities/project.entity';
+import { Portfolio } from '../../portfolio/models/entities/portfolio.entity';
 
 export class ProjectSeeder implements Seeder {
   async run(
@@ -9,9 +10,17 @@ export class ProjectSeeder implements Seeder {
     factoryManager: SeederFactoryManager,
   ): Promise<void> {
     const profileRepository = dataSource.getRepository(Profile);
+    const portfolioRepository = dataSource.getRepository(Portfolio);
     const proyectSeederFactory = factoryManager.get(Project);
     const profile = profileRepository.create({ id: 1 });
 
-    await proyectSeederFactory.saveMany(5, { profile });
+    await proyectSeederFactory.saveMany(3, {
+      profile,
+      portfolio: await portfolioRepository.create({ id: 1 }),
+    });
+    await proyectSeederFactory.saveMany(2, {
+      profile,
+      portfolio: await portfolioRepository.create({ id: 2 }),
+    });
   }
 }
